abbd2d8db9a39eae1812a091e889b2a1620ead46
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-02-13 21:47:13 +0100

    terminato selfwork oggetti3

--- main3.js
+++ main3.js
@@ -1,2 +1,55 @@
-let ciaone = `ciao`;
-console.log(ciaone);
+let bowling = {
+    giocatori : [
+        { nome :`A`, punteggio : []},
+        { nome :`B`, punteggio : []},
+        { nome :`C`, punteggio : []},
+        { nome :`D`, punteggio : []},
+        
+    ],
+    
+    scores :  function () {
+        this.giocatori.forEach(giocatore => { 
+            for (let i = 1; i <= 10; i++) {
+                giocatore.punteggio.push(Math.floor(Math.random()* (10 - 1 + 1) + 1));
+            }
+        })
+    },
+    totalScore : function () {
+        this.giocatori.forEach(giocatore => {
+            let tot = giocatore.punteggio.reduce((acc, num) => acc + num);
+            giocatore.total = tot
+            
+        })
+        this.giocatori.sort((a, b) => b.total - a.total)
+    },
+    add_player : function (name) {
+        let newgiocatore = {nome: name, punteggio: []}
+        for (let i = 1; i <= 10; i++) {
+            newgiocatore.punteggio.push(Math.floor(Math.random()* (10 - 1 + 1) + 1));
+        }
+        this.giocatori.push(newgiocatore);
+        
+        
+    },
+    winner : function () {
+        let vincitore = this.giocatori[0]
+        console.log(`Il vincitore è ${vincitore.nome} con un totale di ${vincitore.total}`);
+        
+    },
+    classifica : function () {
+        this.giocatori.forEach(giocatore => {
+            console.log(`${giocatore.nome} ${giocatore.total} punti totali`);
+            
+        })
+        
+    }
+};
+bowling.scores()
+
+bowling.add_player(`E`)
+
+bowling.totalScore()
+
+bowling.winner()
+
+bowling.classifica()
